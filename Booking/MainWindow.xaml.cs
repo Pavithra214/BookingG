d588db33132f3e3d1f7a92bb7ae82b8ed972a02c
Pavithra214 <pavithra.it214@gmail.com>
2024-12-31 12:30:08 +0530

    Booking

---
+++ Booking/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Booking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+
+    }
+}
